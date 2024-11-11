8155971bbed35a7146dcfeacdfcd4acc7c948c84
msaitoc1 <143106327+msaitoc1@users.noreply.github.com>
2024-11-11 11:47:27 -0800

    Fix .gitignore to exclude .env

--- treadSite/settings.py
+++ treadSite/settings.py
@@ -15,14 +15,14 @@ import os
 import environ
 
 env = environ.Env()
-environ.Env.read_env()
+
 
 
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
